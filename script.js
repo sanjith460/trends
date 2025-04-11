a6aa220c014205484dde45b4d7390996ce14356e
sanjith460 <snjthsaravanan@gmail.com>
2025-04-11 14:44:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function on(){
+	const email=prompt("Enter your email!");
+	const pass=prompt("Enter your password!");
+	let total=alert("your email is : "+email+"your password is : "+pass);
+	alert("This website is Developed by Sanjith.")
+}
